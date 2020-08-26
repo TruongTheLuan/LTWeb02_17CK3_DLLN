6ea26cf7a20aaf9f19b2cc73fc15e4f281f46cfc
ttdat-thecodeguy <ttdat17ck1@gmail.com>
2020-08-26 21:36:03 +0700

    cap nhat auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,12 +2,14 @@ const asyncHandler = require('express-async-handler');
 
 //Middleware
 module.exports = asyncHandler(async function auth(req, res, next){
+    //tim user qua session hoac cookie
     let user = req.session.user || req.cookies['user'];
     res.locals.currentUser = null;
-    
+    //neu khong co user trong session thi quay ve login
     if(!user)
         return res.redirect('/login');
     
+    //neu co thi di tiep 
     req.currentUser = user;
     res.locals.currentUser = user;
    
