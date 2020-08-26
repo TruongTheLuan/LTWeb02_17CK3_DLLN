d994cdc4bb500baf64768de4de332d5776a79556
ttdat-thecodeguy <ttdat17ck1@gmail.com>
2020-08-26 21:40:08 +0700

    cap nhat auth-admin

--- middleware/auth_admin.js
+++ middleware/auth_admin.js
@@ -4,7 +4,8 @@ const asyncHandler = require('express-async-handler');
 module.exports = asyncHandler(async function auth(req, res, next){
     let user = req.session.user || req.cookies['user'];
     res.locals.currentUser = null;
-
+   //neu k co user thi quay ve login
+   //neu co thi kiem tra user do co la admin k neu k quay ve trang chu
     if(!user)
         return res.redirect('/login');
     else
