2c9fdfb531061c3a9ff333e85f49e07f20f5d076
ttdat-thecodeguy <ttdat17ck1@gmail.com>
2020-08-26 21:42:59 +0700

    cap nhat db

--- services/db.js
+++ services/db.js
@@ -2,6 +2,8 @@ const Sequelize = require("sequelize");
 
 // Option 2: Passing parameters separately (other dialects)
 
+//luu connection string la bien moi truong hoac mac dinh
+
 const connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/internetbankingDB' ;
 /*
 const db = new Sequelize('quanlitodos', 'postgres', 'postgres', {
@@ -9,5 +11,7 @@ const db = new Sequelize('quanlitodos', 'postgres', 'postgres', {
     dialect: 'postgres'
 });
 */
+//khai bao sequelize
 const db = new Sequelize(connectionString);
+//export ra ngoai
 module.exports = db;
