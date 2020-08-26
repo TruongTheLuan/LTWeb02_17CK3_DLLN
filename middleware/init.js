24ba6746597f655957f6c81318de5b98d5adf592
ttdat-thecodeguy <ttdat17ck1@gmail.com>
2020-08-26 21:34:20 +0700

    cap nhat init

--- middleware/init.js
+++ middleware/init.js
@@ -1,5 +1,7 @@
-module.exports = function(req, res, next){
+//day la file co chuc nang la se inject vao 1 file khac de goi nhung cai trong no
 
+module.exports = function(req, res, next){
+	//set current user la session hoac cookie
     res.locals.currentUser = req.session.user || req.cookies['user'];
     req.currentUser = req.session.user || req.cookies['user'];
     return next();
